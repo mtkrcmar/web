b5c238483fc4c00fe5843c006e27549943d57349
Michael Krcmarik <michael.krcmarik@gmail.com>
2015-03-01 20:00:11 -0500

    Added in user authentication and login system and shelled in front end website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def index
-    render "layouts/application.html.erb"
+    render text: 'HELLO'
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'static_pages/home'
-  get 'static_pages/app'
-  get 'static_pages/contact'
-  get 'static_pages/about'
-  get 'static_pages/license'
-
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
