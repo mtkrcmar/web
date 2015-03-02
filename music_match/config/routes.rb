1b3ddc31c2075f7f095c91d6fb2d86b40c11a8e5
Michael Krcmarik <michael.krcmarik@gmail.com>
2015-03-01 20:00:11 -0500

    Added in user authentication and login system and shelled in front end website

--- music_match/config/routes.rb
+++ music_match/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/home'
+  get 'static_pages/app'
+  get 'static_pages/contact'
+  get 'static_pages/about'
+  get 'static_pages/license'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
